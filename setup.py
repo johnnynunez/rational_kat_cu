6b0fa09cf9c79f76721badb9edc11497a22a65b1
Johnny <johnnynuca14@gmail.com>
2025-06-26 10:59:03 +0200

    Enhance import handling and update dependencies for 1D and 2D group implementations

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-# setup.py
+# setup.py (Corregido)
 import glob
-from setuptools import setup
+from setuptools import setup, find_packages  # Importar find_packages
+
 
 def get_extensions():
     """
@@ -27,12 +28,34 @@ def get_extensions():
             },
         )
     ]
-    cmdclass = {"build_ext": BuildExtension}
-    return ext_modules, cmdclass
+    return ext_modules, {"build_ext": BuildExtension}
+
 
 ext_modules, cmdclass = get_extensions()
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
+    name="kat_rational",
+    version="0.4",
+    author="adamdad",
+    author_email="yxy_adadm@qq.com",
+    description="CUDA-optimised group-wise rational function for KAT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Adamdad/rational_kat_cu",
+
+    packages=find_packages(exclude=("tests", "example")),
+
+    python_requires=">=3.8",
+    install_requires=[
+        "torch>=2.0",
+        "numpy>=1.26.4",
+        # Instalación condicional de Triton
+        'triton>=2.1.0; sys_platform != "win32"',
+        'triton-windows>=2.1.0; sys_platform == "win32"',
+    ],
     ext_modules=ext_modules,
     cmdclass=cmdclass,
 )
