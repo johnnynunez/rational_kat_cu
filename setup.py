b385131b0c38e44a4e8e61b972e113a169e1f5d6
Johnny <johnnync13@gmail.com>
2025-06-26 09:40:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,52 @@
+# setup.py
 import glob
-from setuptools import setup, find_packages
+from pathlib import Path
+from setuptools import setup
+from setuptools.command.build_ext import build_ext
 
 
-sources = glob.glob('src/*.cpp')+glob.glob('src/*.cu')
+def get_extensions():
+    """
+    Lazily import torch after it is guaranteed to be available
+    and return (ext_modules, cmdclass).
+    """
+    try:
+        from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+    except ImportError as e:
+        raise RuntimeError(
+            "PyTorch must be installed to build the C++/CUDA extension.\n"
+            "Try:  pip install torch --index-url https://download.pytorch.org/whl/cu121"
+        ) from e
 
+    sources = glob.glob("src/*.cpp") + glob.glob("src/*.cu")
+
+    ext_modules = [
+        CUDAExtension(
+            name="kat_rational._cuda",
+            sources=sources,
+            extra_compile_args={
+                "cxx": ["-O3"],
+                "nvcc": ["-O3", "-use_fast_math"],
+            },
+        )
+    ]
+    return ext_modules, {"build_ext": BuildExtension}
+
+
+ext_modules, cmdclass = get_extensions()
 
 setup(
-    name='kat_rational',  # Name of the package
-    version='0.4',  # Version of the package
-    author='adamdad',  # Name of the author
-    author_email='yxy_adadm@qq.com',  # Contact email of the author
-    description='A simple example of a PyTorch extension, implementing a group-wise rational function for kat',  # Short description
-    long_description="""This package provides a PyTorch extension for computing group-wise rational functions. 
-                        It is designed to be used as part of the 'kat' project, enhancing its capabilities in handling
-                        specialized mathematical functions with optimized CUDA support.""",  # Detailed description
-    # ext_modules=[
-    #     CUDAExtension(name='kat_rational_cu', 
-    #                   sources=sources,
-    #                   )
-    # ],
+    name="kat_rational",
+    version="0.4",
+    author="adamdad",
+    author_email="yxy_adadm@qq.com",
+    description="CUDA-optimised group-wise rational function for KAT",
+    long_description=(
+        "A PyTorch C++/CUDA extension that provides fast group-wise rational "
+        "functions used in the *kat* project."
+    ),
+    python_requires=">=3.8",
+    install_requires=["torch>=2.0"],
+    ext_modules=ext_modules,
+    cmdclass=cmdclass,
 )
